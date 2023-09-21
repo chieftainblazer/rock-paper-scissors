e756312925f8ba97a2495ce003048a283d205203
chieftainblazer <110900442+chieftainblazer@users.noreply.github.com>
2023-09-20 21:51:47 -0700

    Add eventlisteners for the buttons and logic for the game
    Added the eventlisteners for the buttons, modified a few lines in the
    playRound function for functionality as well as completing the logic
    for displaying the results of the game. The buttons also stop
    listening once the score of either player is 5.

--- script.js
+++ script.js
@@ -31,42 +31,65 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
+            computerScore++;
+            computerSpan.textContent = computerScore;
             return "You Lose! Paper beats Rock";
         }
         if (computerSelection === "scissors") {
+            playerScore++;
+            playerSpan.textContent = playerScore;
             return "You Win! Rock beats Scissors"
         }
     }
 
     if (playerSelection === "paper") {
         if (computerSelection === "rock") {
+            playerScore++;
+            playerSpan.textContent = playerScore;
             return "You Win! Paper beats Scissors";
         }
         if (computerSelection === "scissors") {
+            computerScore++;
+            computerSpan.textContent = computerScore;
             return "You Lose! Scissors beats Paper"
         }
     }
 
     if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
+            computerScore++;
+            computerSpan.textContent = computerScore;
             return "You Lose! Rock beats Scissors";
         }
         if (computerSelection === "paper") {
+            playerScore++;
+            playerSpan.textContent = playerScore;
             return "You Win! Scissors beats Paper";
         }
     }
 }
 
-function game() {
-    
+function eventFunction(event) {
+    currentResult.textContent = playRound(event.target.value, getComputerChoice());
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5) {
+            finalResult.textContent = "You won the game!";
+        } else {
+            finalResult.textContent = "You lost the game. Opponent won!";
+        }
+        button.forEach(button => button.removeEventListener('click', eventFunction));
+    }
 }
 
 const button = document.querySelectorAll('.button');
+const playerSpan = document.querySelector('#player-score');
+const computerSpan = document.querySelector('#computer-score');
+const currentResult = document.querySelector('#current-result');
+const finalResult = document.querySelector('#final-result');
+
+let playerScore = 0;
+let computerScore = 0;
 
 button.forEach(button => {
-    button.addEventListener('click', event => {
-        console.log(playRound(event.target.value, getComputerChoice()));
-    })
+    button.addEventListener('click', eventFunction);
 })
-
-game();
